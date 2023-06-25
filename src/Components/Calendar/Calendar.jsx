c1801f82d8143c67fa794f245ebedc238143f0cd
dpoeymirou <desirepoey@gmail.com>
2023-06-25 04:09:08 -0300

    Add header css

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -1,76 +1,37 @@
-import React from 'react'
-import { currentMonth, currentYear, months } from '../../utils/months'
-import './Calendar.css'
+import React from "react";
+import { currentMonth, currentYear, months } from "../../utils/months";
+import "./Calendar.css";
 
 const Calendar = () => {
-
-    return (
-        <div>
-            <div className='flex-container'>
-                <div className='month'>
-                    <button className='button'>{'<'}</button>
-                    {months[currentMonth].name} - {currentYear}
-                    <button className='button'>{'>'}</button>
-                </div>
-                <div>
-                    <button className='button'>{'<'}</button>  <button className='button'> {'>'} </button>
-                </div>
-
-            </div>
-            <div className='grid-container'>
-
-                <div className='grid-item'>
-                    Mon
-                </div>
-
-                <div className='grid-item'>
-                    Tue
-                </div>
-
-                <div className='grid-item'>
-                    Wed
-                </div>
-
-                <div className='grid-item'>
-                    Thu
-                </div>
-
-                <div className='grid-item'>
-                    Fri
-                </div>
-
-                <div className='grid-item'>
-                    Sat
-                </div>
-
-                <div className='grid-item'>
-                    Sun
-                </div>
-
-                <div className='grid-item'>
-                    Grilla
-                </div>
-
-                <div className='grid-item'>
-                    Grilla
-                </div>
-
-                <div className='grid-item'>
-
-                    Grilla
-                </div>
-                <div className='grid-item'>
-
-                    Grilla
-                </div>
-                <div className='grid-item'>
-
-                    Grilla
-                </div>
-
-            </div>
+  return (
+    <>
+      <div className="header-grid-container">
+        <div className="month">
+          <button className="button">{"<"}</button>
+          {months[currentMonth].name} - {currentYear}
+          <button className="button">{">"}</button>
         </div>
-    )
-}
+        <div>
+          <button className="button">{"<"}</button>
+          <button className="button"> {">"} </button>
+        </div>
+      </div>
+      <div className="calendar-grid-container">
+        <div className="grid-item">Mon</div>
+        <div className="grid-item">Tue</div>
+        <div className="grid-item">Wed</div>
+        <div className="grid-item">Thu</div>
+        <div className="grid-item">Fri</div>
+        <div className="grid-item">Sat</div>
+        <div className="grid-item">Sun</div>
+        <div className="grid-item">Grilla</div>
+        <div className="grid-item">Grilla</div>
+        <div className="grid-item">Grilla</div>
+        <div className="grid-item">Grilla</div>
+        <div className="grid-item">Grilla</div>
+      </div>
+    </>
+  );
+};
 
-export default Calendar
+export default Calendar;
